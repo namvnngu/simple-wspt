baddd387c68f12771e200800f3f4848847fe890c
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-04-25 00:32:56 +1000

    Finished Project

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let jobID = document.getElementById("job");
 let pElement = document.getElementById("p-value");
 let wElement = document.getElementById("w-value");
 let submitButton = document.getElementById("submit-button");
+let calButton = document.getElementById("cal-button");
+let sortingButton = document.getElementById("sorting-button");
+let renewButton = document.getElementById("renew-button");
+let bodyTable = document.querySelector("table tbody");
+let minWC = document.getElementById("result");
 let table = [];
 
 // Row Addition
@@ -12,7 +17,6 @@ function addRow() {
   let w = wElement.value;
   let row = [id, parseFloat(p), parseFloat(w), parseFloat(w) / parseFloat(p)];
   table.push(row);
-  console.log(table);
 
   if (id != "" && p != "" && w != "") {
     createRow(id, parseFloat(p), parseFloat(w));
@@ -26,10 +30,10 @@ function addRow() {
 }
 
 function createRow(id, p, w) {
+  sortingButton.disabled = false;
   let row = document.createElement("tr");
   let arr = [id, p, w];
   let cell;
-  let bodyTable = document.querySelector("table tbody");
   row.classList.add("d-flex");
 
   for (let i = 0; i < 3; i++) {
@@ -42,8 +46,35 @@ function createRow(id, p, w) {
 }
 
 // Sorting the table
+function sorting() {
+  calButton.disabled = false;
+  if (table != []) {
+    table.sort(function (a, b) {
+      if (a[3] == (0)[3]) {
+        return 0;
+      } else {
+        return a[3] < b[3] ? 1 : -1;
+      }
+    });
+  }
+  bodyTable.textContent = "";
+  table.map(function (row) {
+    createRow(row[0], row[1], row[2]);
+  });
+}
 
 // Min calculation
+function calculateMin() {
+  let result = 0;
+  let c = 0,
+    w = 0;
+  for (let i = 0; i < table.length; i++) {
+    c += table[i][1];
+    w = table[i][2];
+    result += c * w;
+  }
+  minWC.innerHTML = result;
+}
 
 // Events
 window.onload = function () {
@@ -56,4 +87,13 @@ wElement.addEventListener("keyup", function (e) {
     submitButton.click();
     jobID.focus();
   }
+});
+sortingButton.addEventListener("click", sorting);
+calButton.addEventListener("click", calculateMin);
+renewButton.addEventListener("click", function () {
+  table = [];
+  bodyTable.textContent = "";
+  sortingButton.disabled = true;
+  calButton.disabled = true;
+  minWC.innerHTML = "0";
 });
